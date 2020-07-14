162f348e28ede2457394d1ddd852b91e41487ea8
Charles Berman <Charles.Berman@magna.com>
2020-07-14 12:13:14 -0400

    fixed

--- packages/serato-js/src/index.ts
+++ packages/serato-js/src/index.ts
@@ -1,40 +1,9 @@
 
-import Crate from './Crate'
-import Database from './Database';
-
-import { listCrates, listCratesSync } from './listCrates';
-
-import { getDefaultPath } from './util';
-// Singleton for Serato Folder Path (I doubt it'll change during runtime)
-
-/*
-const FOLDER_PATH = getDefaultPath();
-const CRATES_FOLDER = path.join(FOLDER_PATH, "Subcrates");
-const DBFILE = path.join(FOLDER_PATH, "database V2");
-
-const database = Database.getDatabase(DBFILE);
-database.parse();
-
-const crates = listCratesSync(CRATES_FOLDER);
-
-for (let crate of crates) {
-    const songs = crate.getSongPathsSync();
-    for (let song of songs) {
-        console.log('crate');
-    }
-}
-*/
+export { default as Crate } from './Crate';
+export { default as Database } from './Database';
 
 
+export { listCrates, listCratesSync } from './listCrates';
 
-const seratojs = {
-    Crate,
-    Database,
-    getDefaultPath,
-    listCratesSync,
-    listCrates
-};
+export { getDefaultPath } from './util';
 
-
-
-export default seratojs;
